7072d90ec4093a01ab925659b65487b478ef234f
Sam Singer <samuel.singer1@gmail.com>
2018-02-22 18:08:34 -0700

    Actions for retrieving data

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,28 @@ export const getTables = tablesArray => ({
 });
 
 export const fetchTables = () => dispatch => {
+  fetch(`https://check-api.herokuapp.com/tables`, {
+    headers: {
+      Authorization: apiKey
+    }
+  })
+    .then(response => response.json())
+    .then(response => dispatch(getTables(response)))
+    .catch(error => console.log(error));
+};
+
+export const fetchItems = () => dispatch => {
+  fetch(`https://check-api.herokuapp.com/items`, {
+    headers: {
+      Authorization: apiKey
+    }
+  })
+    .then(response => response.json())
+    .then(response => dispatch(getTables(response)))
+    .catch(error => console.log(error));
+};
+
+export const fetchChecks = () => dispatch => {
   fetch(`https://check-api.herokuapp.com/checks`, {
     headers: {
       Authorization: apiKey
