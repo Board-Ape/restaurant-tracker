0e2941a170a290f8e6b565fc016c12f9877ec707
Sam Singer <samuel.singer1@gmail.com>
2018-02-22 17:45:45 -0700

    Move out fetch into actions and create root reducer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import apiKey from '../apikey.js';
+const randomID = require('random-id');
 
 class App extends Component {
 
   componentDidMount() {
-    fetch(`https://check-api.herokuapp.com/restaurant-tracker`, {
-      headers: {
-        Authorization: apiKey
-      }
-    })
-    .then(response => console.log(response))
-    .then(response => console.log(response));
+    const id =randomID();
+    console.log(id);
   }
 
   render() {
