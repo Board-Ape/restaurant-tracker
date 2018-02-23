33fd2713d9374f4e6305fa619d29cb09177ff737
Sam Singer <samuel.singer1@gmail.com>
2018-02-22 18:14:46 -0700

    Get tables data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,17 @@ class App extends Component {
     console.log(id);
   }
 
+  getTables = () => {
+    fetch(`https://check-api.herokuapp.com/checks/d8d66715-c799-4732-b2c1-8d1b79590a6d`, {
+      headers: {
+        Authorization: apiKey
+      }
+    })
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
